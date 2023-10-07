0f4b88b9a8e455bd5449bd2820580bc85fd4ac2c
Priyanka Patel <128116001+Priyankapatel011@users.noreply.github.com>
2023-10-07 10:49:45 +0530

    Add files via upload

---
+++ restaurantConstant.js
@@ -0,0 +1,15 @@
+//in this file will define action types
+
+export const ALL_RESTAURANTS_REQUEST = "ALL_RESTAURANTS_REQUEST";
+export const ALL_RESTAURANTS_SUCCESS = "All_RESTAURANT_SUCCESS";
+export const ALL_RESTAURANTS_FAIL = "ALL_RESTAURANTS_FAIL";
+
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+
+
+export const SORT_BY_RATINGS = "SORT_BY_RATINGS";
+export const SORT_BY_REVIEWS = "SORT_BY_REVIEWS";
+export const TOGGLE_VEG_ONLY = "TOGGLE_VEG_ONLY";
+
+
+
